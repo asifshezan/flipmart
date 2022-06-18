dfa0ab1db89e0de4168ed2a7ad23fed415f86a53
Asif Ahamed <asifshezan7@gmail.com>
2022-06-18 15:19:32 +0600

    dashboard setup

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\AdminController;
+
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function (){
+    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard.index');
+});
+
+require __DIR__.'/auth.php';
