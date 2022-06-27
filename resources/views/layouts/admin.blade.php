05ebfc8a1f77468333aedd296100f830ce911b1a
unknown <asifshezan7@gmail.com>
2022-06-27 23:16:44 +0600

    datatable css & js file add

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,7 +25,12 @@
         <link href="{{asset('contents/admin')}}/css/icons.min.css" rel="stylesheet" type="text/css" />
         <!-- App Css-->
         <link href="{{asset('contents/admin')}}/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- DataTables -->
+        <link href="{{asset('contents/admin')}}/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('contents/admin')}}/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
+        <!-- Responsive datatable examples -->
+        <link href="{{asset('contents/admin')}}/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
@@ -643,11 +648,31 @@
         <!-- Plugins js-->
         <script src="{{asset('contents/admin')}}/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
         <script src="{{asset('contents/admin')}}/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- Required datatable js -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
         <!-- dashboard init -->
         <script src="{{asset('contents/admin')}}/js/pages/dashboard.init.js"></script>
 
         <script src="{{asset('contents/admin')}}/js/app.js"></script>
         <script src="{{asset('contents/admin')}}/js/sweetalert.min.js"></script>
+        <!-- Buttons examples -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/jszip/jszip.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/pdfmake/build/pdfmake.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/pdfmake/build/vfs_fonts.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+
+        <!-- Responsive examples -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+        <!-- Datatable init js -->
+        <script src="{{asset('contents/admin')}}/js/pages/datatables.init.js"></script>
+        <script src="{{asset('contents/admin')}}/js/app.js"></script>
 
     </body>
 
