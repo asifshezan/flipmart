97d236c37b8ab8afec0ee2a7c255b7595e231f33
Asif Ahamed <asifshezan7@gmail.com>
2022-08-04 10:29:59 +0600

    cart add

--- app/Http/Controllers/website/CartController.php
+++ app/Http/Controllers/website/CartController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\website;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Coupon;
+use Darryldecode\Cart\CartCondition;
 use Illuminate\Http\Request;
+use Darryldecode\Cart\Cart;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use function Sodium\Compare;
 
 class CartController extends Controller
 {
-    //
+    public function index(){
+        $allcart = Cart::getContent();
+        return view('website.shopping_cart', compact('allcart'));
+    }
+
+
+    public function store($slug){
+        $product = Product::where('product_status', 1)->where('product_slug', $slug)->firstOrFail();
+        Cart::add([
+            'id' => $product->product_id,
+            'name' => $product->product_name,
+            'price' => $product->product_price,
+            'quantity' => 1,
+            'attributes' => [
+                'product_image' => $product->product_image
+            ]
+        ]);
+        return redirect()->back();
+    }
 }
